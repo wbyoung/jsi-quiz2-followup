c7b3fe33c0d360ab6e8c990226bb47e3e3694b9b
“Andreamsofcake” <andrea.moulding@gmail.com>
2014-09-15 11:45:07 -0700

    got working quiz

--- index.js
+++ index.js
@@ -32,13 +32,8 @@ var newArray = [];
 people.forEach(function(person){
   var mergeName = person.firstName + ' ' + person.lastName;
   newArray.push(mergeName);
-})
-
-
-return newArray
-
-
-
+});
+return newArray;
   // if you're having trouble here, slow down. don't just guess and throw code
   // in. one thing that is often helpful is to write out the steps of what you
   // want to do in comments, then take the comments one by one and translate
--- test/test.js
+++ test/test.js
@@ -36,3 +36,5 @@ describe('fullNames()', function() {
   });
 
 });
+
+//Horray!
